b38cadc827d61774acf49c514eefda966746186b
Arun Khanna <arunkhanna00@gmail.com>
2016-06-27 14:59:30 +0530

    add another cat and add respective names

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-var numberOfClicks = 0;
-$(".cat-pic").click(function(e){
-    // Set a click function on the image to increase a count every time there is a click
-    numberOfClicks += 1
-    // Update the text on the screen corresponding to number of clicks
-    $(".click-display").text("Cat has been clicked " + numberOfClicks + " times");
+// Setup counters for each cat and names
+var catOneClicks = 0;
+var catTwoClicks = 0;
+var catOneName = "Lazy";
+var catTwoName = "Handsome";
+
+// Add cat names to each image
+$(".cat-1").prepend("<h3>" + catOneName + "</h3>");
+$(".cat-2").prepend("<h3>" + catTwoName + "</h3>");
+
+// Add text that displays number of times each cat has been clicked(starting at 0)
+$(".cat-1").append('<h3 class="click-display-1">' + catOneName + ' has been clicked 0 times</h3>');
+$(".cat-2").append('<h3 class="click-display-2">' + catTwoName + ' has been clicked 0 times</h3>')
+
+
+// Everytime each cat has been clicked update its respective counter and update
+// the number that is displayed
+$(".cat-pic-1").click(function(e){
+    catOneClicks += 1
+    $(".click-display-1").text(catOneName + " has been clicked " + catOneClicks + " times");
+});
+
+$(".cat-pic-2").click(function(e){
+    catTwoClicks += 1
+    $(".click-display-2").text(catTwoName + " has been clicked " + catTwoClicks + " times");
 });
 
