e8ddd26574d547494a4cc84a09067d1a234ba5e4
Arun Khanna <arunkhanna00@gmail.com>
2016-07-04 18:45:21 +0530

    refactor code using model-octupus-view paradigm

--- js/script.js
+++ js/script.js
@@ -1,31 +1,127 @@
-// Create an array of clicks for each cat(starting at 0)
-catClicks = [0, 0, 0, 0, 0];
-// Create an array for the names of each cat
-catNames = ["Leo", "Mr Whiskers", "Sunstorm", "Furball", "Tufts"];
+$(function() {
+    var catData = {
+        // Setup array of Cat objects. Each cat object contains a name
+        // and number of clicks
+        catInfo: [
+            {
+                catName: "Leo",
+                catClicks: 0
+            },
+            {
+                catName: "Mr Whiskers",
+                catClicks: 0
+            },
+            {
+                catName: "Sunstorm",
+                catClicks: 0
+            },
+            {
+                catName: "Furball",
+                catClicks: 0
+            },
+            {
+                catName: "Tufts",
+                catClicks: 0
+            }
+        ],
+        // Setup empty object to represent the Cat to be displayed
+        currentCat: {}
+    };
 
-// Setup a for loop to iterate through all of the cats
-for (var catNum = 0; catNum < catClicks.length; catNum++) {
-    // Add each cat name to the display
-    $(".cat-names").append("<h2 id='" + catNum + "'>" + catNames[catNum] + "</h2>")
-    // Create a clicker for each name listed
-    $("#" + catNum).click(function(catNumCopy){
-        return function() {
+    var octupus = {
+        init: function() {
+            // Initialize Octupus function by running
+            // viewCatLinks.init()
+            viewCatLinks.init();
+        },
+
+        clickCatLink: function(catNum) {
+            // When a cat is clicked, add a number and an image link to
+            // the current cat object
+            catData.currentCat.catNum = catNum;
+            catData.currentCat.image = "img/cat-pic-" + catNum + ".jpg";
+        },
+
+        updateCatClicks: function() {
+            // When a cat is clicked increase the clicks of that cat
+            // object
+            catData.catInfo[catData.currentCat.catNum].catClicks++;
+        },
+
+        getCatInfo: function() {
+            // Return the array of cat objects
+            return catData.catInfo;
+        },
+
+        getCatName: function(catNum) {
+            // Return the cat name representing the
+            // cat number in the array of cat objects
+            return catData.catInfo[catNum].catName;
+        },
+
+        getCatImageOfCurrentCat: function() {
+            // Return the image link of the current cat object
+            return catData.currentCat.image;
+        },
+
+        getCatNameOfCurrentCat: function() {
+            // Return the name of the current cat object
+            return catData.catInfo[catData.currentCat.catNum].catName;
+        },
+
+        getCatClicksOfCurrentCat: function() {
+            // Return the number of clicks of the current cat object
+            return catData.catInfo[catData.currentCat.catNum].catClicks;
+        }
+    };
+
+    var viewCatLinks = {
+        init: function() {
+            // Call the render function of the viewCatLinks
+            // function
+            viewCatLinks.render();
+        },
+
+        render: function() {
+            // Iterate through the array of cat objects and display
+            // the cat names. Setup a click handler if any name is clicked
+            for (var catNum = 0; catNum < octupus.getCatInfo().length; catNum++) {
+                $(".cat-names").append("<a href='#'><h2 id='" + catNum + "'>" + octupus.getCatName(catNum) + "</h2></a>");
+                $("#" + catNum).click(function(catNumCopy){
+                    return function() {
+                        octupus.clickCatLink(catNumCopy);
+                        viewCatPic.init();
+                    };
+                }(catNum));
+            }
+        }
+    };
+
+    var viewCatPic = {
+        init: function() {
+            // store the location of the class cat display as this.catDisplay
+            this.catDisplay = $(".cat-display");
+            // render the picture
+            viewCatPic.render();
+        },
+
+        render: function() {
             // Display an image for each cat
-            $(".cat-display").html("<img src=img/cat-pic-" +
-                catNumCopy + ".jpg alt='" + catNames[catNumCopy] + "'>");
+            this.catDisplay.html("<img src=" + octupus.getCatImageOfCurrentCat() + ">");
             // Display the name of the cat on the top
-            $(".cat-display").prepend("<h2>" + catNames[catNumCopy] + "</h2>");
+            this.catDisplay.prepend("<h2>" + octupus.getCatNameOfCurrentCat() + "</h2>");
             // Display the number of times the cat has been clicked
-            $(".cat-display").append("<h2>" + catNames[catNumCopy] + " has been clicked <span class='clickTimes'>" +
-                    catClicks[catNumCopy] + "</span> times</h2>");
+            this.catDisplay.append("<h2>" + octupus.getCatNameOfCurrentCat() + " has been clicked <span class='clickTimes'>" +
+                    octupus.getCatClicksOfCurrentCat() + "</span> times</h2>");
 
             // Create a clicker for each cat image
             $("img").click(function(e){
-                // Update the number of clicks
-                catClicks[catNumCopy] += 1;
+                // Update the number of clicks by calling octupus.updateCatClicks()
+                octupus.updateCatClicks()
                 // Display the updated number of clicks
-                $(".clickTimes").text(catClicks[catNumCopy]);
+                $(".clickTimes").text(octupus.getCatClicksOfCurrentCat());
             });
-        };
-    }(catNum));
-}
+        }
+    };
+    octupus.init();
+}());
