09639adeb71c7b58cde59c03b8935f4c4ab05c2c
Arun Khanna <arunkhanna00@gmail.com>
2016-07-04 22:15:06 +0530

    add an admit button that brings up a form to edit values for each cat

--- js/script.js
+++ js/script.js
@@ -39,10 +39,11 @@ $(function() {
             // When a cat is clicked, add a number and an image link to
             // the current cat object
             catData.currentCat.catNum = catNum;
+            catData.currentCat.catName = catData.catInfo[catNum].catName;
             catData.currentCat.image = "img/cat-pic-" + catNum + ".jpg";
         },
 
-        updateCatClicks: function() {
+        increaseCatClicks: function() {
             // When a cat is clicked increase the clicks of that cat
             // object
             catData.catInfo[catData.currentCat.catNum].catClicks++;
@@ -66,12 +67,27 @@ $(function() {
 
         getCatNameOfCurrentCat: function() {
             // Return the name of the current cat object
-            return catData.catInfo[catData.currentCat.catNum].catName;
+            return catData.currentCat.catName;
         },
 
         getCatClicksOfCurrentCat: function() {
             // Return the number of clicks of the current cat object
             return catData.catInfo[catData.currentCat.catNum].catClicks;
+        },
+
+        updateCatName: function(catName) {
+            // Update the curent cat name
+            catData.currentCat.catName = catName;
+        },
+
+        updateCatImage: function(catImage) {
+            // Update the current cat image
+            catData.currentCat.image = catImage;
+        },
+
+        updateCatClicks: function(catClicks) {
+            // Update the current cat click number(the number will be permanent for each cat)
+            catData.catInfo[catData.currentCat.catNum].catClicks = catClicks;
         }
     };
 
@@ -113,14 +129,42 @@ $(function() {
             // Display the number of times the cat has been clicked
             this.catDisplay.append("<h2>" + octupus.getCatNameOfCurrentCat() + " has been clicked <span class='clickTimes'>" +
                     octupus.getCatClicksOfCurrentCat() + "</span> times</h2>");
+            // Display the admin button
+            this.catDisplay.append("<div class='button-div'><button class='admin-btn'><h2>Admin</h2></button></div>" +
+                "<div class='cat-form'></div>");
 
             // Create a clicker for each cat image
             $("img").click(function(e){
                 // Update the number of clicks by calling octupus.updateCatClicks()
-                octupus.updateCatClicks()
+                octupus.increaseCatClicks()
                 // Display the updated number of clicks
                 $(".clickTimes").text(octupus.getCatClicksOfCurrentCat());
             });
+
+            $(".admin-btn").click(function(e) {
+                // Setup a form with 3 text input boxes and two buttons(cancel and submit) when the
+                // admin button is clicked
+                $(".cat-form").html("<form><h2>Name:<input type='text' class='admin-entry catName' value='" + octupus.getCatNameOfCurrentCat() + "'></h2>" +
+                    "<h2>Image:<input type='text' class='admin-entry catImage' value='" + octupus.getCatImageOfCurrentCat() + "'></h2>" +
+                    "<h2>Clicks:<input type='text' class='admin-entry catClicks' value='" + octupus.getCatClicksOfCurrentCat() + "'></h2>" +
+                    "<div class='button-div'><input type='button' value='Submit' class='submit-btn'>" +
+                    "<input type='button' value='Cancel' class='cancel-btn'></div></form>");
+
+                $(".cancel-btn").click(function(e) {
+                    // When the cancel button is clicked clear the form
+                    $(".cat-form").empty();
+                });
+                $(".submit-btn").click(function(e){
+                    // When the submit button is clicked update the name, image and number of clicks
+                    // for the current cat. Then render the Cat Display again with the new values
+                    octupus.updateCatName($(".catName").val());
+                    octupus.updateCatImage($(".catImage").val());
+                    octupus.updateCatClicks($(".catClicks").val());
+                    viewCatPic.render();
+
+                });
+            });
+
         }
     };
     octupus.init();
